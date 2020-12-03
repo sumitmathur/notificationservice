7d2931dc01dc40bec03b6569b4a63a314a57c1a4
Sumit Mathur <Sumit_Mathur@intuit.com>
2020-12-03 14:20:47 +0530

    notificationservice

--- src/main/java/com/sumit/notificationservice/configuration/AppConfiguration.java
+++ src/main/java/com/sumit/notificationservice/configuration/AppConfiguration.java
@@ -2,9 +2,7 @@ package com.sumit.notificationservice.configuration;
 
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.stereotype.Component;
 
 import java.util.concurrent.Executor;
 
